33308b508e4498710c37801107f6d107f68a6012
Jarrod McEnroe <jwmcenroe@gmail.com>
2018-06-19 12:32:52 -0700

    Added mongoose schema, create campground route, show campground route

--- app.js
+++ app.js
@@ -1,39 +1,91 @@
-var express = require('express');
-var app = express();
-var bodyParser = require('body-parser');
+var express = require('express'),
+    app = express(),
+    bodyParser = require('body-parser'),
+    mongoose = require('mongoose');
+
+    mongoose.connect('mongodb://localhost/yelp_camp');
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.set('view engine', 'ejs');
 
-var campgrounds = [
-    {name: 'Salmon Creek', image: 'https://pixabay.com/get/ec31b90f2af61c22d2524518b7444795ea76e5d004b014439df2c37fa5e8bc_340.jpg'},
-    {name: 'Whistler Gorge', image: 'https://pixabay.com/get/e83db50a21f4073ed1584d05fb1d4e97e07ee3d21cac104497f8c37ba1efb0b1_340.jpg'},
-    {name: 'Lake Nas', image: 'https://pixabay.com/get/ea31b10929f7063ed1584d05fb1d4e97e07ee3d21cac104497f8c37ba1efb0b1_340.jpg'}
-];
+// schema setup
+var campgroundSchema = new mongoose.Schema({
+    name: String,
+    image: String,
+    description: String
+});
+
+var Campground = mongoose.model('Campground', campgroundSchema);
+
+// Campground.create(
+//     {
+//         name: 'Nowhereville USA', 
+//         image: 'https://pixabay.com/get/e83db50a21f4073ed1584d05fb1d4e97e07ee3d21cac104497f9c27ca7e8b6bd_340.jpg',
+//         description: 'This is in the middle of nowhere, you stupid douchknuckle'
+//     }, function(err, campground){
+//         if(err){
+//             console.log(err);
+//         } else {
+//             console.log('Newly-created campground: ');
+//             console.log(campground);
+//         }
+//     });
+
+// =======================================
+// *** ROUTES ***
+// =======================================
 
 // landing page
 app.get('/', function(req, res){
     res.render('landing');
 });
 
-// shows all campgrounds
+// *** INDEX Route - shows all campgrounds ***
 app.get('/campgrounds', function(req, res){
-    res.render('campgrounds', {campgrounds:campgrounds});
+    // Get all campgrounds from DB
+    Campground.find({}, function(err, allCampgrounds){
+        if(err){
+            console.log(err);
+        } else {
+            res.render('index', {campgrounds:allCampgrounds});
+        }
+    });
 });
 
-// shows new campground form
+// *** CREATE Route - add new campground to DB ***
+app.post('/campgrounds', function(req, res){
+    // get data from form and add to campgrounds array
+    var name = req.body.name;
+    var image = req.body.image;
+    var desc = req.body.description;
+    var newCampground = {name: name, image: image, description: desc};
+    // Create new campground and save to DB
+    Campground.create(newCampground, function(err, newlyCreated){
+        if(err){
+            console.log(err);
+        } else {
+            // redirect back to campgrounds page
+            res.redirect('/campgrounds');
+        }
+    });
+});
+
+// *** NEW Route - shows new campground form to create new campground ***
 app.get('/campgrounds/new', function(req, res){
     res.render('new.ejs');
 });
 
-app.post('/campgrounds', function(req, res){
-    // get data from form and add to cmapgrounds array
-    var name = req.body.name;
-    var image = req.body.image;
-    var newCampground = {name: name, image: image}
-    campgrounds.push(newCampground);
-    // redirect back to campgrounds page
-    res.redirect('/campgrounds');
+// *** SHOW Route - shows more info about one campground ***
+app.get('/campgrounds/:id', function(req, res){
+    // find campground with provided ID
+    Campground.findById(req.params.id, function(err, foundCampground){
+        if(err){
+            console.log(err);
+        } else {
+            // render show template with that campground
+            res.render('show', {campground: foundCampground});
+        }
+    });
 });
 
 app.listen(3000, process.env.IP, function() {
